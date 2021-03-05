8354bd6d2a4bf76e25362730afeb54d6755b14d3
thommccarthy <mccarttj@gmail.com>
2021-03-05 18:58:35 -0500

    added style modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,37 @@
 import React from "react"
 import { Link } from "gatsby"
+//destructure from module
+import { link } from "../styles/header.module.scss"
 
 const Header = () => {
   return (
-    <div>
+    <header>
       <h1>My Portfolio</h1>
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link className={link} to="/">
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link className={link} to="/about">
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link className={link} to="/blog">
+              Blog
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link className={link} to="/contact">
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
-    </div>
+    </header>
   )
 }
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import Header from "./header"
 import Footer from "./footer"
 import "../styles/index.scss"
+import "../styles/layout.module.scss"
 
 const Layout = props => {
   return (

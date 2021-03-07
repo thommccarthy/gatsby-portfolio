aff3d3bd3f87b83ca8ba4fe727ab0e326d348f68
thommccarthy <mccarttj@gmail.com>
2021-03-07 13:04:33 -0500

    styled blog parent page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import * as footerStyles from "./footer.module.scss"
 
 const Footer = () => {
   const data = useStaticQuery(graphql`
@@ -13,9 +14,9 @@ const Footer = () => {
   `)
 
   return (
-    <div>
-      <p>Created by {data.site.siteMetadata.author}, Copyright 2021</p>
-    </div>
+    <footer className={footerStyles.footer}>
+      Created by {data.site.siteMetadata.author}, Copyright 2021
+    </footer>
   )
 }
 
